ae7bd6ea685aa470e12ca38e53231ab518926452
EirikNilsson <eirik.haugennilsson@elev.ga.ntig.se>
2025-11-27 14:10:58 +0100

    color change

--- src/App.vue
+++ src/App.vue
@@ -54,17 +54,8 @@
   <TodoList v-if="!loading && !error" :todos="todos" @toggleCompletion="toggleTodo" @delete="deleteTodo"/> -->
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+body {
+  background-color: gainsboro;
 }
 </style>
